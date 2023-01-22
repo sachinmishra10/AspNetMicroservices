ee1c018dd7b8e5f40dc06fe8fe17bc6edc5a1cf8
sachinmishra10m <sachin.mishra10@gmail.com>
2023-01-22 22:45:41 +0530

    implement discount grpc service in Basket.API

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcServices.cs
@@ -0,0 +1,21 @@
+﻿using Discount.GRPC.Protos;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcServices
+    {
+        private readonly DiscountService.DiscountServiceClient _discountService;
+
+        public DiscountGrpcServices(DiscountService.DiscountServiceClient discountService)
+        {
+            this._discountService = discountService;
+        }
+        
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var getdiscountRequest = new GetDiscountRequest { ProductName = productName };
+           return  await _discountService.GetDiscountAsync(getdiscountRequest);
+        }
+    }
+}
