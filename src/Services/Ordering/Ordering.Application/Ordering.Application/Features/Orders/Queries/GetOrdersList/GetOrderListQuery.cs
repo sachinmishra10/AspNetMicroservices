96f00a118236170aa386dd3619195db103296983
sachinmishra10m <sachin.mishra10@gmail.com>
2023-01-26 15:39:43 +0530

    Checkin Oredring API

---
+++ src/Services/Ordering/Ordering.Application/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrderListQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrderListQuery:IRequest<List<OrderVM>>
+    {
+        public string UserName { get; set; }
+        public GetOrderListQuery(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrderListQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistance;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrderListQueryHandler : IRequestHandler<GetOrderListQuery, List<OrderVM>>
+    {
+        private readonly IOrderRepository _orderrepository;
+        private readonly IMapper _mapper;
+
+        public GetOrderListQueryHandler(IOrderRepository repository,IMapper mapper)
+        {
+            this._orderrepository = repository;
+            this._mapper = mapper;
+        }
+        public async Task<List<OrderVM>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        {
+           var orderlist=await _orderrepository.GetOrderByUserName(request.UserName);
+            return _mapper.Map<List<OrderVM>>(orderlist);
+        }
+    }
+}
